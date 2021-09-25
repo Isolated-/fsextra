c359313f71f37f98878c0be7b33e70097b7907ff
Michael Palmer <michaelpalmer53@gmail.com>
2021-09-25 20:08:59 +0100

    updates extensions public interface

--- src/extensions/mod.rs
+++ src/extensions/mod.rs
@@ -1,4 +1,13 @@
+/// extended `File` traits + implementations
+pub mod file;
+
+/// extends `Metadata` traits + implementations
 pub mod metadata;
+
 mod tests;
 
+/// export for convience
+pub use file::FileExtended;
+
+/// export for convience
 pub use metadata::MetadataExtended;
