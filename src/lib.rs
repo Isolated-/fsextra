f41dabc87ae19e9df0a6a9139e99d6bfe3223917
Michael Palmer <michaelpalmer53@gmail.com>
2021-09-25 20:07:10 +0100

    updates public interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
+/// a collection of extensions for filesystem operations
 pub mod extensions;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+/// a collection of generic types/bounds
+pub mod generics;
+
+#[cfg(feature = "crypto")]
+/// support for cryptographic operations (requires `crypto` features)
+pub mod crypto;
