c8525dac3b7b400f3e6e3092cc1b43fbc245edc6
Michael Palmer <michaelpalmer53@gmail.com>
2021-09-27 21:32:27 +0100

    updates file locations in crypto tests

--- src/extensions/tests/file_crypto_tests.rs
+++ src/extensions/tests/file_crypto_tests.rs
@@ -7,7 +7,7 @@ mod file_crypto_tests {
 
     #[test]
     fn file_digest_returns_sha256() {
-        let mut file = File::open("test_data/test_01.txt").unwrap();
+        let mut file = File::open("test_data/files/test_01.txt").unwrap();
         let digest = file.digest(DigestAlgorithm::Sha2).unwrap();
         let expected = "7509e5bda0c762d2bac7f90d758b5b2263fa01ccbc542ab5e3df163be08e6ca9";
 
@@ -17,7 +17,7 @@ mod file_crypto_tests {
 
     #[test]
     fn file_digest_returns_sha512() {
-        let mut file = File::open("test_data/test_01.txt").unwrap();
+        let mut file = File::open("test_data/files/test_01.txt").unwrap();
         let digest = file.digest(DigestAlgorithm::Sha5).unwrap();
         let expected = "db9b1cd3262dee37756a09b9064973589847caa8e53d31a9d142ea2701b1b28abd97838bb9a27068ba305dc8d04a45a1fcf079de54d607666996b3cc54f6b67c";
 
@@ -27,7 +27,7 @@ mod file_crypto_tests {
 
     #[test]
     fn file_digest_returns_sha384() {
-        let mut file = File::open("test_data/test_01.txt").unwrap();
+        let mut file = File::open("test_data/files/test_01.txt").unwrap();
         let digest = file.digest(DigestAlgorithm::Sha384).unwrap();
         let expected = "d33d40f7010ce34aa86efd353630309ed5c3d7ffac66d988825cf699f4803ccdf3f033230612f0945332fb580d8af805";
 
