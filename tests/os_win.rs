37e9c951946d54e6d7ca8f26566314932ea1f275
Michael Palmer <michaelpalmer53@gmail.com>
2021-09-27 07:04:44 +0100

    adds feature flags to tests

--- tests/os_win.rs
+++ tests/os_win.rs
@@ -1,9 +1,11 @@
 mod common;
 
 #[cfg(windows)]
+#[cfg(feature = "os")]
 use fsextra::os::win::is_executable;
 
 #[cfg(windows)]
+#[cfg(feature = "os")]
 #[test]
 fn is_executable_returns_expected_output_from_inputs() {
     let tests = common::consume_os_win_exec_tests();
