dd913685f16315dc76a57e53a8c765dd0c36d099
Michael Palmer <michaelpalmer53@gmail.com>
2021-09-27 02:54:32 +0100

    adds Sha384 test coverage

--- src/extensions/tests/file_crypto_tests.rs
+++ src/extensions/tests/file_crypto_tests.rs
@@ -2,7 +2,7 @@
 #[cfg(feature = "crypto")]
 mod file_crypto_tests {
     use crate::extensions::file::{DigestAlgorithm, FileExtended};
-    use ring::digest::{SHA256_OUTPUT_LEN, SHA512_OUTPUT_LEN};
+    use ring::digest::{SHA256_OUTPUT_LEN, SHA384_OUTPUT_LEN, SHA512_OUTPUT_LEN};
     use std::fs::File;
 
     #[test]
@@ -25,5 +25,13 @@ mod file_crypto_tests {
         assert_eq!(hex::encode(digest), expected);
     }
 
-    // add more
+    #[test]
+    fn file_digest_returns_sha384() {
+        let mut file = File::open("test_data/test_01.txt").unwrap();
+        let digest = file.digest(DigestAlgorithm::Sha384).unwrap();
+        let expected = "d33d40f7010ce34aa86efd353630309ed5c3d7ffac66d988825cf699f4803ccdf3f033230612f0945332fb580d8af805";
+
+        assert_eq!(digest.len(), SHA384_OUTPUT_LEN);
+        assert_eq!(hex::encode(digest), expected);
+    }
 }
