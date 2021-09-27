7d6d7e10ebb1500b7bc8d87afa0c7d2ce38a825a
Michael Palmer <michaelpalmer53@gmail.com>
2021-09-27 03:52:26 +0100

    updates documentation for digest mod

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,2 +1,15 @@
-/// provides support for calculating digests (`sha256`/`sha512`) - requires `crypto` feature
+/// Supports calculating digests using various algorithms (requires `crypto` feature)
+///
+/// This interface was improved and extended in `v0.3.0`.
+///
+/// ## Supported Algorithms
+///
+/// - `SHA-256`
+/// - `SHA-384`
+/// - `SHA-512`
+///
+/// ## Note
+///
+/// Version `v0.2.0` exposed a different interface, it is recommended to use `Digest`.
+/// If you require the `v0.2.0` interface, you'll find it in `fsextra::crypto::digest::legacy`.
 pub mod digest;
