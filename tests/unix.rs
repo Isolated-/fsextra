c7edb6f061d626635dffd1c7159979264d01878b
Michael Palmer <michaelpalmer53@gmail.com>
2021-09-27 20:27:41 +0100

    adds coverage for unix is_executable (loaded from file)

--- tests/unix.rs
+++ tests/unix.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 
 #[cfg(unix)]
 #[test]
+/// this also tests directories (previously in metadata_tests, now removed)
 fn is_executable_returns_expected_output_from_inputs() {
     let tests = common::consume_unix_exec_tests();
 
@@ -13,5 +14,24 @@ fn is_executable_returns_expected_output_from_inputs() {
         let metadata = file.metadata().unwrap();
 
         assert_eq!(metadata.is_executable(), test.expected);
+    }
+}
+
+#[test]
+#[cfg(unix)]
+fn metadata_is_executable_does_not_change_file() {
+    let tests = common::consume_unix_exec_tests();
+
+    for test in tests {
+        let file = File::open(&test.path).unwrap();
+        let metadata = file.metadata().unwrap();
+
+        assert_eq!(metadata.is_executable(), test.expected);
+
+        let comparison = File::open(&test.path).unwrap().metadata().unwrap();
+
+        assert_eq!(metadata.len(), comparison.len());
+        assert_eq!(metadata.modified().unwrap(), comparison.modified().unwrap());
+        assert_eq!(metadata.accessed().unwrap(), comparison.accessed().unwrap());
     }
 }
