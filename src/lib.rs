a1f4af08d18d68c45aa8e3367a96ed557636c6f5
Michael Palmer <michaelpalmer53@gmail.com>
2021-09-27 02:56:11 +0100

    adds compatibility notice

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-/// a collection of extensions for filesystem operations
+/// A collection of extensions for filesystem operations
+///
+/// ## Compatibility
+///
+/// - Version `v0.3.0` is tested on Unix-based operating systems.
+/// - Version `v0.2.0` is untested to work on any other system (although Rust may not complain).
 pub mod extensions;
 
 /// a collection of generic types/bounds
